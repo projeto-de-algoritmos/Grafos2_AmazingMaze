fefe949b4d6ce38da586f3c6e9be55f17e13fdd3
davirany <davirany@gmail.com>
2023-05-10 16:29:26 -0300

    create AboutUs component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Main } from './components/Main';
+import AboutUs from './components/AbouUs';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/about-us' element={<AboutUs />} />
       </Routes>
     </div>
   );
