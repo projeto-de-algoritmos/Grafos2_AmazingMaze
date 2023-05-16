74ffe63ac1e97bb045b3ff1e05762d0dc9acca62
pehenobra2 <peheguibra@gmail.com>
2023-05-16 01:31:11 -0300

    implementando dijkstra para encontrar o menor caminho até o final do labirinto

--- src/components/MazeGame.js
+++ src/components/MazeGame.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Maze from "./Maze";
-import './styles/MazeGame.css'
+import "./styles/MazeGame.css";
 
 export default function MazeGame() {
 
@@ -11,12 +11,12 @@ export default function MazeGame() {
     [1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1],
     [1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
     [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
     [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1],
-    [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1],
     [1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 2],
-    [1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 2],
+    [1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
   ];
 
@@ -24,8 +24,60 @@ export default function MazeGame() {
     row: 0,
     column: 1,
   });
-
   const [isMazeCompleted, setIsMazeCompleted] = useState(false);
+  const [distances, setDistances] = useState([]);
+  const [shortestPath, setShortestPath] = useState([]);
+  const [foundShortestPath, setFoundShortestPath] = useState(false);
+  const [pathSquares, setPathSquares] = useState([]);
+
+  useEffect(() => {
+    const shortestPath = dijkstra(maze, playerPosition);
+    setDistances(shortestPath);
+
+    const pathSquares = shortestPath.map((path) => `${path.row}-${path.column}`);
+    setPathSquares(pathSquares);
+  }, [maze, playerPosition]);
+
+
+  const dijkstra = (maze, start) => {
+    const visited = new Set();
+    const queue = [[start.row, start.column, 0]];
+    const shortestPath = [];
+
+    while (queue.length > 0) {
+      const [row, column, distance] = queue.shift();
+      const key = `${row},${column}`;
+
+      if (visited.has(key)) continue;
+      visited.add(key);
+
+      shortestPath.push({ row, column, distance });
+
+      // Check neighboring cells
+      const neighbors = [
+        [row - 1, column], // Up
+        [row + 1, column], // Down
+        [row, column - 1], // Left
+        [row, column + 1], // Right
+      ];
+
+      for (const [nextRow, nextColumn] of neighbors) {
+        if (
+          nextRow >= 0 &&
+          nextRow < maze.length &&
+          nextColumn >= 0 &&
+          nextColumn < maze[0].length &&
+          !visited.has(`${nextRow},${nextColumn}`)
+        ) {
+          if (maze[nextRow][nextColumn] === 0) {
+            queue.push([nextRow, nextColumn, distance + 1]);
+          }
+        }
+      }      
+    }
+
+    return shortestPath;
+  };
 
 
   const handleKeyDown = (event) => {
@@ -79,19 +131,29 @@ export default function MazeGame() {
       default:
         break;
     }
-  };
 
-  const { row, column } = playerPosition;
-  if (row === 9 && column === 41) {
-    alert("Parabéns! Você concluiu o labirinto!");
-  }
-
-  useEffect(() => {
-    const { row, column } = playerPosition;
-    if (maze[row][column] === 2) {
-      setIsMazeCompleted(true);
+    const currentDistance = distances.find(
+      (d) =>
+        d && d.row === playerPosition.row && d.column === playerPosition.column
+    );
+  
+    const distanceToGoal = currentDistance ? currentDistance.distance : undefined;
+  
+    if (
+      distanceToGoal === shortestPath[shortestPath.length - 1]?.distance &&
+      playerPosition.row === 9 &&
+      playerPosition.column === 41
+    ) {
+      alert(
+        "Parabéns! Você encontrou o menor caminho possível e concluiu o labirinto!"
+      );
+    } else if (distanceToGoal === shortestPath[shortestPath.length - 1]?.distance) {
+      alert("Parabéns! Você encontrou o menor caminho possível!");
+    } else if (playerPosition.row === 9 && playerPosition.column === 41) {
+      alert("Parabéns! Você concluiu o labirinto!");
     }
-  }, [maze, playerPosition]);
+
+  };
 
   const handleMazeCompleted = () => {
     setIsMazeCompleted(true);
@@ -100,7 +162,11 @@ export default function MazeGame() {
   return (
     <div className="maze-game" onKeyDown={handleKeyDown} tabIndex="0">
       {isMazeCompleted ? (
-        <span className="maze-completed">Labirinto concluído!</span>
+        <span className="maze-completed">
+          {foundShortestPath
+            ? "Labirinto concluído e menor caminho encontrado!"
+            : "Labirinto concluído!"}
+        </span>
       ) : (
         <div>
           <div style={{ display: "flex" }}>
@@ -108,6 +174,7 @@ export default function MazeGame() {
               maze={maze}
               playerPosition={playerPosition}
               handleMazeCompleted={handleMazeCompleted}
+              pathSquares={pathSquares}
             />
           </div>
         </div>
