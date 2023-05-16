f7ac6fbc264c27c7a3afab4f04be652be50851ea
davirany <davirany@gmail.com>
2023-05-15 23:12:25 -0300

    atualizando o mapa

--- src/components/Maze.js
+++ src/components/Maze.js
@@ -5,6 +5,8 @@ function Maze({ maze, playerPosition }) {
     const mazeRows = maze.map((mazeRow, rowIndex) => {
         const mazeCells = mazeRow.map((mazeCell, columnIndex) => {
             const isPlayer = rowIndex === playerPosition.row && columnIndex === playerPosition.column;
+            const isStartCell = rowIndex === 0 && columnIndex === 1;
+            const isEndCell = rowIndex === 9 && columnIndex === 41;
 
             return (
                 <div
@@ -12,7 +14,7 @@ function Maze({ maze, playerPosition }) {
                     style={{
                         width: "30px",
                         height: "30px",
-                        backgroundColor: isPlayer ? "#56CCF2" : mazeCell === 0 ? "#F2F2F2" : "#222",
+                        backgroundColor: isPlayer ? "#56CCF2" : isStartCell || isEndCell ? "red" : mazeCell === 0 ? "#F2F2F2" : "#222",
                         border: "1px solid #56CCF2",
                         boxSizing: "border-box",
                     }}
--- src/components/MazeGame.js
+++ src/components/MazeGame.js
@@ -1,146 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Maze from "./Maze";
 import './styles/MazeGame.css'
 
-function gerarLabirinto() {
-    const linhas = 20;
-    const colunas = 20;
-  
-    // Criar matriz vazia
-    const labirinto = [];
-    for (let i = 0; i < linhas; i++) {
-      const linha = [];
-      for (let j = 0; j < colunas; j++) {
-        linha.push(0);
-      }
-      labirinto.push(linha);
-    }
-  
-    const direcoes = [
-      { linha: -1, coluna: 0 },
-      { linha: 0, coluna: 1 },
-      { linha: 1, coluna: 0 },
-      { linha: 0, coluna: -1 }
-    ];
-  
-    const pilha = [{ linha: 1, coluna: 1 }]; // Posição inicial
-    labirinto[1][1] = 1; // Definir a posição inicial como caminho
-  
-    while (pilha.length > 0) {
-      const atual = pilha[pilha.length - 1];
-      const vizinhos = [];
-  
-      for (const direcao of direcoes) {
-        const vizinhoLinha = atual.linha + direcao.linha * 2;
-        const vizinhoColuna = atual.coluna + direcao.coluna * 2;
-  
-        if (
-          vizinhoLinha >= 0 &&
-          vizinhoLinha < linhas &&
-          vizinhoColuna >= 0 &&
-          vizinhoColuna < colunas &&
-          labirinto[vizinhoLinha][vizinhoColuna] === 0
-        ) {
-          vizinhos.push({ linha: vizinhoLinha, coluna: vizinhoColuna });
-        }        
-      }
-  
-      if (vizinhos.length === 0) {
-        pilha.pop();
-      } else {
-        const vizinho = vizinhos[Math.floor(Math.random() * vizinhos.length)];
-        const caminhoLinha = (atual.linha + vizinho.linha) / 2;
-        const caminhoColuna = (atual.coluna + vizinho.coluna) / 2;
-  
-        labirinto[caminhoLinha][caminhoColuna] = 1; // Abrir caminho entre células
-        labirinto[vizinho.linha][vizinho.coluna] = 1; // Mover para a próxima célula
-  
-        pilha.push({ ...vizinho });
-      }
-    }
-  
-    return labirinto;
-  }
+export default function MazeGame() {
+  const maze = [
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
 
-  export default function MazeGame() {
-    const [maze, setMaze] = useState([]);
-    const [playerPosition, setPlayerPosition] = useState({
-      row: 1,
-      column: 1,
-    });
-  
-    const handleKeyDown = (event) => {
-      const { key } = event;
-  
-      switch (key) {
-        case "ArrowUp":
-          if (
-            playerPosition.row > 0 &&
-            maze[playerPosition.row - 1][playerPosition.column] === 0
-          ) {
-            setPlayerPosition((prev) => ({
-              ...prev,
-              row: prev.row - 1,
-            }));
-          }
-          break;
-        case "ArrowDown":
-          if (
-            playerPosition.row < maze.length - 1 &&
-            maze[playerPosition.row + 1][playerPosition.column] === 0
-          ) {
-            setPlayerPosition((prev) => ({
-              ...prev,
-              row: prev.row + 1,
-            }));
-          }
-          break;
-        case "ArrowLeft":
-          if (
-            playerPosition.column > 0 &&
-            maze[playerPosition.row][playerPosition.column - 1] === 0
-          ) {
-            setPlayerPosition((prev) => ({
-              ...prev,
-              column: prev.column - 1,
-            }));
-          }
-          break;
-        case "ArrowRight":
-          if (
-            playerPosition.column < maze[0].length - 1 &&
-            maze[playerPosition.row][playerPosition.column + 1] === 0
-          ) {
-            setPlayerPosition((prev) => ({
-              ...prev,
-              column: prev.column + 1,
-            }));
-          }
-          break;
-        default:
-          break;
-      }
-    };
-  
-    useEffect(() => {
-        const generatedMaze = gerarLabirinto(); // Corrigido para chamar a função gerarLabirinto
-        setMaze(generatedMaze);
-      }, []);
-    
-    return (
-    <div
-        className="maze-game"
-        onKeyDown={handleKeyDown}
-        tabIndex="0"
-        style={{ width: `${maze?.[0]?.length * 30}px` }}
-    >
-        <div>
-        <p>Clique no labirinto e depois</p>
-        <p>Use as teclas de direção para mover o jogador</p>
+  const [playerPosition, setPlayerPosition] = useState({
+    row: 0,
+    column: 1,
+  });
+
+  const handleKeyDown = (event) => {
+    const { key } = event;
+
+    switch (key) {
+      case "ArrowUp":
+        if (playerPosition.row > 0 && maze[playerPosition.row - 1][playerPosition.column] === 0) {
+          setPlayerPosition((prev) => ({
+            ...prev,
+            row: prev.row - 1,
+          }));
+        }
+        break;
+      case "ArrowDown":
+        if (playerPosition.row < maze.length - 1 && maze[playerPosition.row + 1][playerPosition.column] === 0) {
+          setPlayerPosition((prev) => ({
+            ...prev,
+            row: prev.row + 1,
+          }));
+        }
+        break;
+      case "ArrowLeft":
+        if (playerPosition.column > 0 && maze[playerPosition.row][playerPosition.column - 1] === 0) {
+          setPlayerPosition((prev) => ({
+            ...prev,
+            column: prev.column - 1,
+          }));
+        }
+        break;
+      case "ArrowRight":
+        if (playerPosition.column < maze[0].length - 1 && maze[playerPosition.row][playerPosition.column + 1] === 0) {
+          setPlayerPosition((prev) => ({
+            ...prev,
+            column: prev.column + 1,
+          }));
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="maze-game" onKeyDown={handleKeyDown} tabIndex="0">
+      <div>
         <div style={{ display: "flex" }}>
-            <Maze maze={maze} playerPosition={playerPosition} />
+          <Maze maze={maze} playerPosition={playerPosition} />
         </div>
-        </div>
+      </div>
     </div>
-    );
+  );
 }
